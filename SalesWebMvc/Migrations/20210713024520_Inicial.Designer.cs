ccde8453c65ee6d76d6363a9ea276fc8b4af3985
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2021-07-13 12:20:53 -0300

    Other entities and second migration

--- SalesWebMvc/Migrations/20210713024520_Inicial.Designer.cs
+++ SalesWebMvc/Migrations/20210713024520_Inicial.Designer.cs
@@ -3,7 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
 
 namespace SalesWebMvc.Migrations
 {
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -10,7 +10,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
 
 namespace SalesWebMvc
 {
